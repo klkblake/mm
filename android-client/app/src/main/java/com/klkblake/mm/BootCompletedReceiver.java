6a0c5cb58a4dc784edd172f87024a48b6ba95599
Kyle Blake <kyle@klkblake.com>
2015-11-29 00:12:43 +1030

    Start MessageService on boot

---
+++ android-client/app/src/main/java/com/klkblake/mm/BootCompletedReceiver.java
@@ -0,0 +1,14 @@
+package com.klkblake.mm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            context.startService(new Intent(context, MessageService.class));
+        }
+    }
+}
